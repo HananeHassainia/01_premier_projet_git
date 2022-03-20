ed6fad4b6110af795ec83385f18617793af595dd
Hanane Hassainia <hassainiahanane@gmail.com>
2022-03-20 23:05:39 +0100

    Ajout d'un constructeur

--- Employe.java
+++ Employe.java
@@ -1,10 +1,15 @@
 public class Employe{ 
-//Commentaire java 
-//commentaire 
-private String nom;
-private String prenom;
-private	int age;
-private String telephone;
-private String adresse;
+	//Commentaire java 
+	//commentaire 
+	private String nom;
+	private String prenom;
+	private	int age;
+	private String telephone;
+	private String adresse;
+
+	// ctor
+	public Employe(){
+	
+	}
 
 }
